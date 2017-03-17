a6394f731d84820bf5d162844e0da337aa996541
Herczog Attila <herczogattil@gmail.com>
2017-03-17 09:57:34 +0100

    Translate login with i18n.

---
+++ src/locale/messages.hu.ts
@@ -0,0 +1,24 @@
+export const TRANSLATION_HU = `<?xml version="1.0" encoding="UTF-8" ?>
+<xliff version="1.2" xmlns="urn:oasis:names:tc:xliff:document:1.2">
+  <file source-language="en" datatype="plaintext" original="ng2.template">
+    <body>
+      <trans-unit id="67fc3fe0566f8700b84a908c35497759db78addc" datatype="html">
+        <source>Username:</source>
+        <target>Felhasználónév:</target>
+      </trans-unit>
+      <trans-unit id="4434eb250f44b5effd90253c0c9f2bf1e69ec13f" datatype="html">
+        <source>Password:</source>
+        <target>Jelszó:</target>
+      </trans-unit>
+      <trans-unit id="cfc2f436ec2beffb042e7511a73c89c372e86a6c" datatype="html">
+        <source>Register</source>
+        <target>Regisztráció</target>
+      </trans-unit>
+      <trans-unit id="6765b4c916060f6bc42d9bb69e80377dbcb5e4e9" datatype="html">
+        <source>Login</source>
+        <target>Belépés</target>
+      </trans-unit>
+    </body>
+  </file>
+</xliff>
+`;
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,26 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, LOCALE_ID, TRANSLATIONS_FORMAT, TRANSLATIONS } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
+import { TRANSLATION_EN } from './locale/messages';
+import { TRANSLATION_HU } from './locale/messages.hu';
 
 // depending on the env mode, enable prod mode or add debugging modules
 if (process.env.ENV === 'build') {
   enableProdMode();
 }
 
+const locale = navigator.language;
+const translations = locale === 'hu' ? TRANSLATION_HU : TRANSLATION_EN;
+
 export function main() {
-  return platformBrowserDynamic().bootstrapModule(AppModule);
+  return platformBrowserDynamic().bootstrapModule(AppModule,
+      {
+        providers: [
+          { provide: TRANSLATIONS, useValue: translations },
+          { provide: TRANSLATIONS_FORMAT, useValue: 'xlf' },
+          { provide: LOCALE_ID, useValue: locale }
+        ]
+      });
 }
 
 if (document.readyState === 'complete') {
