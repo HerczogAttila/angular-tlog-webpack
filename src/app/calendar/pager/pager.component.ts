78e0c9665d0e647158b3e828667ae2f2db0c251f
Herczog Attila <herczogattil@gmail.com>
2017-03-21 16:27:31 +0100

    Use glyphicon.

--- src/app/calendar/pager/pager.component.ts
+++ src/app/calendar/pager/pager.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'my-pager',
     templateUrl: 'pager.component.html',
+    styleUrls: ['pager.component.scss'],
 })
 
 export class PagerComponent {
